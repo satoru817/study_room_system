aa85774c7912b2c15e556df5e74aced9f36d48b9
Satoru Kurita <kurita@lvi.co.jp>
2025-10-19 13:27:58 +0900

    no message

---
+++ StudyRoomReservation/src/main/java/org/example/studyroomreservation/StudyRoomReservationApplication.java
@@ -0,0 +1,13 @@
+package org.example.studyroomreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRoomReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyRoomReservationApplication.class, args);
+    }
+
+}
